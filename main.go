14e992b8e70011480509490efade8ff51c1475aa
Failattu <kalle.sukeltaja@gmail.com>
2015-03-19 22:47:17 +0200

    ADDED Get functionality

--- main.go
+++ main.go
@@ -3,5 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
+	"net/http"
+	"io/ioutil"
 )
+func server(){
+
+}
+
+func getReq(address string){
+	resp, err := http.Get(address)
+	if err != nil {
+		fmt.Printf("ERROR IN GET")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	s := string(body)
+	return s
+}
+
+func postReq(address string){
+
+}
+
+func main() {
+	fmt.Printf("hello, world\n")
+	
+	if len(os.Args) > 1 {
+		if os.Args[1] == "get" {
+			var address = os.Args[2]
+			getReq(address)
+		}else if os.Args[1] == "post"{
+			var address = os.Args[2]
+			postReq(address)
+		}else if os.Args[1] == "server"{
+			server()
+		}else{
+			fmt.Printf("Goodbye\n")
+		}
+	}
+}
