c771ef09efec340e0f5f8e694957f60ef4126b39
Failattu <kalle.sirkesalo@eficode.com>
2015-07-24 21:27:53 +0300

    Initial MySQL example

--- main.go
+++ main.go
@@ -8,15 +8,61 @@ import (
 	"log"
 	"html"
 	"io"
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql"
 )
 func server(){
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	http.HandleFunc("/mysqlw", func(w http.ResponseWriter, r *http.Request) {
+		writeSQL()
+	})
+	http.HandleFunc("/mysqlr", func(w http.ResponseWriter, r *http.Request) {
+		readSQL()
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	})
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
+func readSQL(){
+	con, err := sql.Open("mysql", "test:test@/test")
+	if err != nil { }
+	var (
+	    PersonID int
+	    LastName string
+			FirstName string
+			Address string
+			City string
+	)
+	rows, err := con.Query("SELECT PersonID, LastName , FirstName, Address, City FROM Persons")
+	if err != nil {
+	    log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+	    err := rows.Scan(&PersonID, &LastName,&FirstName,&Address,&City)
+	    if err != nil {
+	        log.Fatal(err)
+	    }
+	    log.Println(PersonID, LastName,FirstName,Address,City)
+			fmt.Print(PersonID, LastName,FirstName,Address,City)
+	}
+	err = rows.Err()
+	if err != nil {
+	    log.Fatal(err)
+	}
 
+	defer con.Close()
+}
+func writeSQL(){
+	con, err := sql.Open("mysql", "test:test@/test")
+	if err != nil {}
+	//Persons(PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));"
+	_, err = con.Exec("INSERT INTO Persons (PersonID, LastName , FirstName, Address, City) VALUES (0, 'test','vesa','pallo', 'forssa');")
+	fmt.Printf( "Kirjotusta")
+	defer con.Close()
+}
 func getReq(address string){
 	resp, err := http.Get(address)
 	if err != nil {
@@ -43,7 +89,7 @@ func postReq(address string){
 
 func main() {
 	fmt.Printf("hello, world\n")
-	
+
 	if len(os.Args) > 1 {
 		if os.Args[1] == "get" {
 			var address = os.Args[2]
